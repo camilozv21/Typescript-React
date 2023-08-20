9669ba61d0dd5ba6a8e8e83be6b08b3e393ebaab
camilozv21 <camilozv21@gmail.com>
2023-08-20 10:23:52 -0500

    change structure of files

---
+++ features/tuples.ts
@@ -0,0 +1,18 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 80]
+const tes: Drink = ['brown', false, 0]
+
+const carSpcs: [number, number] = [400, 3354];
+
+const carStats = {
+    hoserpower: 400,
+    weight: 3354
+};
