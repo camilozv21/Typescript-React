ffa261abe4f3525892914a71cb9efa0810dc0a1a
camilozv21 <camilozv21@gmail.com>
2023-08-25 14:37:57 -0500

    search users

--- react_project/rts/src/index.tsx
+++ react_project/rts/src/index.tsx
@@ -1,17 +1,18 @@
 import ReactDOM from "react-dom/client";
-import GuestList from "./state/GuestList";
+// import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
- 
 const el = document.getElementById("root");
- 
+
 const root = ReactDOM.createRoot(el!);
- 
+
 const App = () => {
   return (
     <div>
-      <GuestList />
+      {/* <GuestList /> */}
+      <UserSearch />
     </div>
   );
 };
- 
+
 root.render(<App />);
---
+++ react_project/rts/src/state/UserSearch.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const users = [
+  { name: "Luisa", age: 23 },
+  { name: "Camilo", age: 22 },
+  { name: "Daniel", age: 44 },
+  { name: "Sophia", age: 32 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
